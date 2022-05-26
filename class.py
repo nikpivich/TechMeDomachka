e9799f17f55c330af12073f17672041ed1e7c6b6
nikita.pivovarchik <pivichnikita@gmail.com>
2022-05-26 16:47:31 +0300

    class2

---
+++ class.py
@@ -0,0 +1,57 @@
+import time
+
+
+class Auto:
+
+    def __init__(self, brand: str, age: int, mark: str, color: str = None, weight: int = None):
+        self.brand = brand
+        self.age = age
+        self.color = color
+        self.mark = mark
+        self.weight = weight
+
+    def move(self):
+        return 'move'
+
+    def stop(self):
+        return 'stop'
+
+    def birthday(self):
+        self.age += 1
+        return self.age
+
+
+class Truck(Auto):
+
+    def __init__(self, max_load: int, brand: str, age: int, mark: str):
+        super().__init__(brand, age, mark)
+        self.max_load = max_load
+
+    def move(self):
+        print('attention')
+        print('move')
+
+    def load(self):
+        time.sleep(1)
+        print('time')
+        time.sleep(1)
+
+
+class Car(Auto):
+
+    def __init__(self, max_speed: int, brand: str, age: int, mark: str):
+        super().__init__(brand, age, mark)
+        self.max_speed = max_speed
+
+    def move(self):
+        print('move')
+        print(f'max speed is {self.max_speed}')
+
+
+a = Auto('VW', 20, 'Passat', 'black', 2000)
+b = Truck(1500, 'VW', 20, 'Passat', )
+c = Car(222, 'Audi', 12, 'A8', )
+
+b.move()
+b.load()
+c.move()
